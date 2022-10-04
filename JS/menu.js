458856f7157b45cafbd39e54bf58c94f971b9fa0
Alipio Gomes <alipiotec@gmail.com>
2022-10-04 17:13:23 -0300

    reset

--- JS/menu.js
+++
@@ -1,9 +0,0 @@
-const menu = document.querySelector('#menu');
-
-menu.addEventListener('click', function(){
-    const menuList = document.querySelector('#menuList');
-
-    menuList.classList.toggle('menuOff');
-    menuList.classList.toggle('menuOn');
-});
-
