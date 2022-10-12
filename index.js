4f103d5f3c6e7c6cfbffec22285705b6964f829c
Alipio Gomes <alipiotec@gmail.com>
2022-10-12 02:03:16 -0300

    respomsividade e design

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const light = document.querySelector('#light');
 const css = document.getElementById('style');
+const memu = document.getElementById('menu');
+
 
 light.addEventListener('click',() => {
     const href = css.href;
@@ -8,7 +10,24 @@ light.addEventListener('click',() => {
     if(css.href == `${address}style.css`) css.href = `/assets/css/styleDark.css`;
     else css.href = `/assets/css/style.css`;
 });
-/*
+
+menu.addEventListener('click', ()=>{
+    const menumbile = document.querySelector('.menumobile');
+    if(menumbile.style.display === "none") return menumbile.style.display = "flex";
+    menumbile.style.display = "none";
+});
+
+
+
+
+
+
+
+
+
+
+
+
 function start() {
     const hora = new Date;
     if(hora.getHours() < 5 || hora.getHours() >= 17){
@@ -17,4 +36,3 @@ function start() {
 };
 
 start();
-*/
