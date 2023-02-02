c1bcd52dabd082ffd7d4261b995d1a6e28223316
Alipio Gomes <alipiotec@gmail.com>
2023-02-02 14:59:51 -0300

    ajustes nas habilidades

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const light = document.querySelector('#light');
 const css = document.getElementById('style');
 const memu = document.getElementById('menu');
+const mgview = document.querySelector('.mangroveview');
 
+mgview.addEventListener('click', ()=>{
+    const prototype = document.querySelector('#prototypemg');
+    const linkmg = document.querySelector("#linkmg");
+    if(prototype.style.display == "none") {
+        prototype.style.display = "block";
+        linkmg.style.display = "none";
+    }else{
+        prototype.style.display = "none";
+        linkmg.style.display = "block";    
+    }
+});
 
 light.addEventListener('click',() => {
     const href = css.href;
     const point = Number(String(href).lastIndexOf('/'));
     const address = `${String(href).slice(0,point+1)}`;
-    if(css.href == `${address}style.css`) css.href = `/assets/css/styleDark.css`;
+    if(css.href == `${address}style.css`)  css.href = `/assets/css/styleDark.css`;
     else css.href = `/assets/css/style.css`;
 });
 
@@ -16,17 +28,6 @@ menu.addEventListener('click', ()=>{
     if(menumbile.style.display === "none") return menumbile.style.display = "flex";
     menumbile.style.display = "none";
 });
-
-
-
-
-
-
-
-
-
-
-
 
 function start() {
     const hora = new Date;
